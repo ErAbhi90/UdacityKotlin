02ed64ff485aeb66b0f930476c4087b9e9ab7d25
ErAbhi90 <er.abhithakral@gmail.com>
2019-04-14 16:33:00 +0530

    Dice Roller - Initial Commit

---
+++ app/src/main/java/abhi/mobiledev/in/udacitydiceroller/MainActivity.kt
@@ -0,0 +1,12 @@
+package abhi.mobiledev.`in`.udacitydiceroller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
