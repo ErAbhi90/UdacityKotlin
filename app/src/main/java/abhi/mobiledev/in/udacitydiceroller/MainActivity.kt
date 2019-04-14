51845dde929faa1a5e5d8e418425edc841beddaa
ErAbhi90 <er.abhithakral@gmail.com>
2019-04-14 17:08:52 +0530

    Random Value 1to6 on button Click

--- app/src/main/java/abhi/mobiledev/in/udacitydiceroller/MainActivity.kt
+++ app/src/main/java/abhi/mobiledev/in/udacitydiceroller/MainActivity.kt
@@ -2,8 +2,10 @@ package abhi.mobiledev.`in`.udacitydiceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+        //rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "ButtonClicked", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "ButtonClicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+
         }
+    }
+
+    private fun rollDice() {
+        var resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
